f7053ea992658e3551c4632fae55499ae2d2bf68
Kuon <cuongbuimanh1203@gmail.com>
2023-05-12 05:57:59 +0700

    fix error

--- giaotiep/giaotiep.ino
+++ giaotiep/giaotiep.ino
@@ -1,3 +1,5 @@
+#include <ArduinoHttpClient.h>
+#include "certs.h"
 #include <Arduino_JSON.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
@@ -7,8 +9,8 @@ const char* ssid = "P110B";
 const char* password = "tumotdenchin";
 
 //Your Domain name with URL path or IP address with path
-// String serverName = "http://garbagecanserver.onrender.com/smartcan";
-String serverName = "http://192.168.1.21:3000/smartcan";
+// String serverName = "https://garbagecanserver.onrender.com";
+String serverName = "http://192.168.1.21:3000";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -24,18 +26,18 @@ typedef struct {
   bool full_state;
 } Payload;
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
+
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
   delay(500);
   Serial.print('r');
@@ -44,56 +46,69 @@ void setup() {
 void loop() {
   // sCmd.readSerial();
   if (Serial.available() > 0) {
-                // đọc chữ liệu
-                text = Serial.read();
+    // đọc chữ liệu
+    text = Serial.read();
 
-                if(char(text) == 'f'){
-                  Serial.println("da send thanh cong trang thai day");
-                  sendData(true);
-                }
-                if(char(text) == 'e'){
-                  Serial.println("da send thanh cong trang thai trong");
-                  sendData(false);
-                }
-        }
+    if (char(text) == 'f') {
+      Serial.println("da send thanh cong trang thai day");
+      sendData(true, serverName + "/smartcan?id=640d3b665c50b0cfb0a0cd3c");
+    }
+    if (char(text) == 'e') {
+      Serial.println("da send thanh cong trang thai trong");
+      sendData(false, serverName + "/smartcan?id=640d3b665c50b0cfb0a0cd3c");
+    }
+  }
 }
 
-void sendData(bool state){
-  if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
+void sendData(bool state, String domain) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+    String serverPath = domain;
+    // client.setFingerprint(fingerprint_sni_cloudflaressl_com);
+    // Your Domain name with URL path or IP address with path
+    http.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+    http.setRedirectLimit(100);
 
-      String serverPath = serverName + "?id=640d3b665c50b0cfb0a0cd3c";
-      
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverPath.c_str());
-      
-      http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-      http.addHeader("Content-Type", "application/json");
-      http.addHeader("Accept", "*/*");
-      http.addHeader("Connection", "keep-alive");  
-      // If you need Node-RED/server authentication, insert user and password below
-      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-        
-      String data = state?"{\"full_state\":true}":"{\"full_state\":false}";
-      
-      // Send HTTP GET request
-      int httpResponseCode = http.PUT(data);
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
+    http.begin(client, serverPath.c_str());
+    http.addHeader("Content-Type", "application/json; charset=utf-8");
+    http.addHeader("Accept", "*/*");
+    http.addHeader("Connection", "keep-alive");
+    // If you need Node-RED/server authentication, insert user and password below
+    //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+    String data = state ? "{\"full_state\":true}" : "{\"full_state\":false}";
+
+    // Send HTTP GET request
+    int httpResponseCode = http.PUT(data);
+
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      if (httpResponseCode == 302 || httpResponseCode == 307) {
+        Serial.print("location: ");
+        Serial.println(http.getLocation());
+        sendData(state, http.getLocation());
       }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+      Serial.println(http.getLocation());
+      String l = "location";
+
+      if (httpResponseCode == 302 || httpResponseCode == 307) {
+        // http.responseStatusCode();
+        // Serial.println(http.headers());
+        // for(int i = 0;i<http.headers();i++){
+        //   Serial.println(http.headerName(i));
+        // }
       }
-      // Free resources
-      http.end();
     }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
+    // Free resources
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
 }
--- thungrac/thungrac.ino
+++ thungrac/thungrac.ino
@@ -8,25 +8,18 @@ SoftwareSerial serial2(RX, TX);
 
 #define UP true
 #define DOWN false
-
 #define dir1 10
 #define pwm1 3
 #define dir2 11
 #define pwm2 6
-
 #define trigPin 4
 #define echoPin 5
-
 #define tiemcan 8
-
 #define servoPin 7
-
 #define ht1 A0
 #define ht2 A1
-
 #define emtyled A2
 #define processLed A3
-
 #define speed 100
 
 
@@ -44,6 +37,12 @@ int timeOpen = 0;
 long duration = 0;
 int distance = 0;
 bool espReady = false;
+
+void controllMotor1(bool dir, int pwm);
+void controllMotor2(bool dir, int pwm);
+void caculateDistance();
+void controlEp();
+
 void setup() {
   pinMode(dir1, OUTPUT);
   pinMode(pwm1, OUTPUT);
@@ -59,15 +58,15 @@ void setup() {
   serial2.begin(9600);
   Serial.begin(9600);
   while (!espReady) {
-  if(Serial.available()){
-    int tg = Serial.read();
-    if(char(tg) == 'r') {
-      espReady = true;
-    } 
+    if (Serial.available()) {
+      int tg = Serial.read();
+      if (char(tg) == 'r') {
+        espReady = true;
+      }
+    }
+    serial2.println("e");
   }
-  serial2.println('e');
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
   int tg = millis() - old;
@@ -113,7 +112,7 @@ void caculateDistance() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration / 2) / 29.1;
-  
+
   if (distance >= 400 || distance <= 0) {
     Serial.println("Out of range");
   } else {
@@ -122,23 +121,23 @@ void caculateDistance() {
   }
 }
 
-void controlEp(){
-  if(run){
+void controlEp() {
+  if (run) {
     serial2.print('f');
-    if(timeEp == 0){
-      controllMotor1(DOWN,speed);
-      controllMotor2(DOWN,speed);
+    if (timeEp == 0) {
+      controllMotor1(DOWN, speed);
+      controllMotor2(DOWN, speed);
       timeEp++;
     }
-    if(timeEp >= 10){
-      controllMotor1(UP,!digitalRead(ht1)?0:speed);
-      controllMotor2(UP,!digitalRead(ht2)?0:speed);
-      if(analogRead(pwm1) == 0 && analogRead(pwm2) == 0){
+    if (timeEp >= 10) {
+      controllMotor1(UP, !digitalRead(ht1) ? 0 : speed);
+      controllMotor2(UP, !digitalRead(ht2) ? 0 : speed);
+      if (analogRead(pwm1) == 0 && analogRead(pwm2) == 0) {
         run = false;
         timeEp = 0;
         serial2.print('e');
-        }
-    }else{
+      }
+    } else {
       timeEp++;
     }
   }
